424cedd715dd9a8ead05f39d6f6f468c1059fcb1
Elfi_Y <elfiyang16@gmail.com>
2021-05-06 10:31:34 +0100

    feat: add cognito config

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import Amplify from "aws-amplify";
 import config from "./config";
 import * as serviceWorker from "./serviceWorker";
 
+Amplify.configure({
+  Auth: {
+    mandatorySignId: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+});
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
